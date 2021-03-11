924f7291aa24f45735bea804429ed943579b4565
Sullivan008 <kozakpeter0424@gmail.com>
2021-03-11 21:10:56 +0100

    [FrontEnd] * Created the BasicData Component.   * This component contains the basic data of the input field (for example counts of words or sentences).   * This component is just a template for now.   * Defiend the basic-data-component in app.component.html.file. * Added new styles for words-counter.component.

---
+++ WordsCounter-FrontEnd/src/app/modules/words-counter/container/components/basic-data/basic-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'words-counter-basic-data',
+  templateUrl: './basic-data.component.html',
+  styleUrls: ['./basic-data.component.scss'],
+})
+export class BasicDataComponent {
+  public constructor() {}
+}
--- WordsCounter-FrontEnd/src/app/modules/words-counter/words-counter.module.ts
+++ WordsCounter-FrontEnd/src/app/modules/words-counter/words-counter.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { BasicDataComponent } from './container/components/basic-data/basic-data.component';
 import { WordsCounterComponent } from './container/words-counter.component';
 import { WordsCounterRoutingModule } from './words-counter-routing.module';
 
 @NgModule({
-  declarations: [WordsCounterComponent],
+  declarations: [WordsCounterComponent, BasicDataComponent],
   imports: [WordsCounterRoutingModule, SharedModule],
 })
 export class WordsCounterModule {}
