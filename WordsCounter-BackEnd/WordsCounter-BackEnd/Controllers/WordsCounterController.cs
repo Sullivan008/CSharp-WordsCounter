bedef446fffe3fe4fb3c1a3243989b73bf1e0807
Sullivan008 <kozakpeter0424@gmail.com>
2021-03-23 11:25:31 +0100

    [BackEnd] * Modified the launchSettings.json. * Created WordsCounterController.

---
+++ WordsCounter-BackEnd/WordsCounter-BackEnd/Controllers/WordsCounterController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Web.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{api-version:apiVersion}/[controller]")]
+    public class WordsCounterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WordsCounterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
