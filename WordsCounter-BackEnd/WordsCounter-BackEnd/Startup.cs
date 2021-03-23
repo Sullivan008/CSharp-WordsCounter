554c9344922b32a40323903a5e9243d561d61bc5
Sullivan008 <kozakpeter0424@gmail.com>
2021-03-23 09:58:50 +0100

    [BackEnd] * Defined and configured the Swagger package and API versioning.

--- WordsCounter-BackEnd/WordsCounter-BackEnd/Startup.cs
+++ WordsCounter-BackEnd/WordsCounter-BackEnd/Startup.cs
@@ -1,13 +1,13 @@
 using Application.DataAccessLayer.Context;
 using Application.Web.Core.Configurations;
 using Application.Web.Core.Extensions;
+using Application.Web.Core.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace Application.Web
 {
@@ -31,30 +31,24 @@ namespace Application.Web
             services.ConfigureReadOnlyDbContext();
             services.ConfigureMediatR();
 
+            services.ConfigureSwaggerServices();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WordsCounter_BackEnd", Version = "v1" });
-            });
+
+            services.AddSwaggerGen();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IApiVersionDescriptionProvider provider, IWebHostEnvironment env)
         {
             app.AddGlobalErrorHandlerMiddleware();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WordsCounter_BackEnd v1"));
-            }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.ConfigureSwagger(provider);
 
             app.UseEndpoints(endpoints =>
             {
