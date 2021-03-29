cc5468b0a34208449573f94e7051325fc6988104
Sullivan008 <kozakpeter0424@gmail.com>
2021-03-29 18:45:55 +0200

    [FrontEnd] * Removed unnecessary DropDown Directive.

--- WordsCounter-FrontEnd/src/app/shared/shared.module.ts
+++ WordsCounter-FrontEnd/src/app/shared/shared.module.ts
@@ -7,21 +7,18 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FontAwesomeIconsModule } from './modules/fontawesome-icons.module';
 
-import { DropDownDirective } from './directives/dropdown.directive';
-
 import { HeaderComponent } from './utilities/header/container/header.component';
 import { FooterComponent } from './utilities/footer/container/footer.component';
 import { CustomAccordionComponent } from './utilities/custom-accordion/custom-accordion.component';
 import { RibbonToastrComponent } from './utilities/ribbon-toastr/container/ribbon-toastr.component';
 
 @NgModule({
-  declarations: [DropDownDirective, RibbonToastrComponent, HeaderComponent, FooterComponent, CustomAccordionComponent],
+  declarations: [RibbonToastrComponent, HeaderComponent, FooterComponent, CustomAccordionComponent],
   imports: [RouterModule, ReactiveFormsModule, CommonModule, HttpClientModule, FontAwesomeModule, AccordionModule],
   exports: [
     RouterModule,
     ReactiveFormsModule,
     CommonModule,
-    DropDownDirective,
     FontAwesomeIconsModule,
     FontAwesomeModule,
     HeaderComponent,
